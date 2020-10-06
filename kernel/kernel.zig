16827a8f463a305e5c99abfaf0125cfc6ecae565
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2020-10-07 00:55:29 +0200

    serial: Add serial port support

--- kernel/kernel.zig
+++ kernel/kernel.zig
@@ -2,17 +2,19 @@ const x86 = @import("arch.zig").x86;
 const std = @import("std");
 const VGAConsole = x86.vga.VGAConsole;
 
-var console: ?VGAConsole = null;
+var vga_console: ?VGAConsole = null;
 
 pub fn printk(comptime fmt: []const u8, args: var) void {
-    var con: *VGAConsole = &console.?;
-    con.outStream().print(fmt, args) catch |err| {};
+    var vgacon: *VGAConsole = &vga_console.?;
+    const com1 = x86.serial.SerialPort(1);
+    vgacon.outStream().print(fmt, args) catch |err| {};
+    com1.outStream().print(fmt, args) catch |err| {};
 }
 
 export fn kmain() void {
-    console = VGAConsole.init();
+    vga_console = VGAConsole.init();
+
     printk("Booting the kernel...\n", .{});
     printk("CR3: 0x{x}\n", .{x86.read_cr3()});
     printk("CPU Vendor: {}\n", .{x86.get_vendor_string()});
-    const cpuid_info = x86.cpuid(0x0, 0x0);
 }
