f76e9e9fe92da673dda580a01e39bc0bf6125565
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2020-10-11 22:14:12 +0200

    build: silence grub-mkrescue

--- mkiso.sh
+++ mkiso.sh
@@ -9,7 +9,7 @@ TMPDIR=$(mktemp -d)
 mkdir -p $TMPDIR/boot/grub
 cp $BUILD_DIR/kernel $TMPDIR/boot/kernel.elf
 cp grub.cfg $TMPDIR/boot/grub/grub.cfg
-grub-mkrescue -o kernel.iso $TMPDIR
+grub-mkrescue -o kernel.iso $TMPDIR 2> /dev/null
 
 
 rm -rf $TMPDIR
