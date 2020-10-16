ace0d3c887b4051ea2daed0b938db56cb1f048d3
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2020-10-16 19:46:02 +0200

    build: move options to arch specific function

--- build.zig
+++ build.zig
@@ -1,32 +1,13 @@
-const std = @import("std");
 const builtin = @import("builtin");
+const std = @import("std");
 const Builder = std.build.Builder;
 const CrossTarget = std.zig.CrossTarget;
 const Arch = std.Target.Cpu.Arch;
 const CpuFeature = std.Target.Cpu.Feature;
 
 fn build_x86_64(kernel: *std.build.LibExeObjStep) void {
-    kernel.setLinkerScriptPath("kernel/arch/x86/linker.ld");
-    kernel.addAssemblyFile("kernel/arch/x86/boot.S");
-}
-
-fn buildArch(kernel: *std.build.LibExeObjStep, arch: std.Target.Cpu.Arch) void {
-    switch (arch) {
-        Arch.x86_64 => {
-            build_x86_64(kernel);
-        },
-        else => {},
-    }
-}
-
-pub usingnamespace CpuFeature.feature_set_fns(std.Target.x86.Feature);
-
-pub fn build(b: *Builder) void {
-    const arch = Arch.x86_64;
-    const mode = b.standardReleaseOptions();
-
     const cross_target = CrossTarget{
-        .cpu_arch = arch,
+        .cpu_arch = Arch.x86_64,
         .cpu_model = CrossTarget.CpuModel.baseline,
         .cpu_features_sub = featureSet(&[_]std.Target.x86.Feature{
             .cmov,
@@ -46,14 +27,35 @@ pub fn build(b: *Builder) void {
         .os_tag = std.Target.Os.Tag.freestanding,
         .abi = std.Target.Abi.none,
     };
+    kernel.setTarget(cross_target);
 
+    kernel.setLinkerScriptPath("kernel/arch/x86/linker.ld");
+    kernel.addAssemblyFile("kernel/arch/x86/boot.S");
+}
+
+fn buildArch(kernel: *std.build.LibExeObjStep, arch: std.Target.Cpu.Arch) void {
+    switch (arch) {
+        Arch.x86_64 => {
+            build_x86_64(kernel);
+        },
+        else => {
+            std.debug.warn("Invalid architecture", .{});
+        },
+    }
+}
+
+pub usingnamespace CpuFeature.feature_set_fns(std.Target.x86.Feature);
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
     const kernel = b.addExecutable("kernel", "kernel/kernel.zig");
+
     kernel.setBuildMode(mode);
     kernel.code_model = builtin.CodeModel.kernel;
-    buildArch(kernel, arch);
-
-    kernel.setTarget(cross_target);
     kernel.strip = true;
+
+    const arch = Arch.x86_64;
+    buildArch(kernel, arch);
 
     kernel.setOutputDir("build/");
     b.default_step.dependOn(&kernel.step);
