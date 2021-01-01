dbd3b857016bd093ac6683db6fabf03b28b358a2
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2021-01-02 00:19:56 +0100

    More fixes for APIC

--- kernel/arch/x86/apic.zig
+++ kernel/arch/x86/apic.zig
@@ -1,3 +1,5 @@
+const kernel = @import("root");
+const mm = kernel.mm;
 const x86 = @import("../x86.zig");
 const logger = x86.logger.childOf(@typeName(@This()));
 const APIC_MSR = x86.APIC_BASE;
@@ -11,12 +13,27 @@ pub fn init() void {
     if (!apic_enabled()) {
         @panic("APIC is disabled!");
     }
-    // TODO remap APIC
-    logger.log("APIC is at {x}\n", .{APIC_MSR.read()});
+
+    const maxphyaddr = x86.cpuid(0x80000008, 0).eax & 0xff;
+    logger.log("Maxphy is {}\n", .{maxphyaddr});
+    const one: u64 = 1;
+    const mask: u64 = ((one << @intCast(u6, maxphyaddr)) - 1) - ((1 << 12) - 1);
+
+    const apic_base = mm.PhysicalAddress.new(((APIC_MSR.read() & mask)));
+    logger.debug("LAPIC is at {}\n", .{apic_base});
 
     const apic_id = (x86.cpuid(1, 0).ebx >> 24) & 0xff;
     logger.log("LAPIC ID {x}\n", .{apic_id});
 
-    const pat = x86.cpuid(1, 0).eax;
-    logger.log("CPUID.1.EAX: {x}\n", .{pat});
+    const io_region = mm.kernel_vm.map_io(apic_base, 0x1000) catch |err| {
+        logger.err("Failed to map IO memory for APIC");
+        return;
+    };
+
+    const lapic = kernel.mmio.DynamicMMIORegion.init(io_region.base.value);
+    const id_register = lapic.Reg(u32, 0x20);
+    const version_register = lapic.Reg(u32, 0x30);
+
+    logger.debug("ID reg: {x}\n", .{id_register.read()});
+    logger.debug("Version reg: {x}\n", .{version_register.read()});
 }
