6f3d7f033fa9a256bd7c7592b2a89e4cb11761cd
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2021-02-21 21:38:33 +0100

    lib: Add spinlock

---
+++ kernel/lib/spinlock.zig
@@ -0,0 +1,37 @@
+const builtin = @import("builtin");
+
+pub const SpinLock = struct {
+    state: State,
+
+    const State = enum {
+        Unlocked,
+        Locked,
+    };
+
+    pub const Held = struct {
+        spinlock: *SpinLock,
+
+        pub fn release(self: Held) void {
+            @atomicStore(State, &self.spinlock.state, .Unlocked, .Release);
+        }
+    };
+
+    pub fn init() SpinLock {
+        return SpinLock{ .state = .Unlocked };
+    }
+
+    pub fn tryAcquire(self: *SpinLock) callconv(.Inline) ?Held {
+        return switch (@atomicRmw(State, &self.state, .Xchg, .Locked, .Acquire)) {
+            .Unlocked => Held{ .spinlock = self },
+            .Locked => null,
+        };
+    }
+
+    pub fn acquire(self: *SpinLock) callconv(.Inline) Held {
+        while (true) {
+            if (self.tryAcquire()) |held| {
+                return held;
+            }
+        }
+    }
+};
