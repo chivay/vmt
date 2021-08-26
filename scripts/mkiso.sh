8e4372958d2ce0e1c97da2a5e19442e7e51538f0
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2021-08-26 02:15:43 +0200

    Move grub.cfg to scripts/

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -4,6 +4,6 @@ TMPDIR=$(mktemp -d)
 
 mkdir -p $TMPDIR/boot/grub
 cp $KERNEL_ELF $TMPDIR/boot/kernel.elf
-cp grub.cfg $TMPDIR/boot/grub/grub.cfg
+cp ./scripts/grub.cfg $TMPDIR/boot/grub/grub.cfg
 grub-mkrescue -o $(dirname $KERNEL_ELF)/kernel.iso $TMPDIR 2> /dev/null
 rm -rf $TMPDIR
