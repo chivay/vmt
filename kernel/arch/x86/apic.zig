2117ce33ed1010a726e8c396ca7a4936e9940afb
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2022-01-23 00:53:45 +0100

    x86/apic: Add register accessors

--- kernel/arch/x86/apic.zig
+++ kernel/arch/x86/apic.zig
@@ -31,6 +31,54 @@ pub fn getLapicId() u8 {
     return @truncate(u8, (x86.cpuid(1, 0).ebx >> 24));
 }
 
+pub fn setDCR(divisor: u8) void {
+    const reg = lapic.Reg(u32, 0x3e0);
+    const value: u32 = switch (divisor) {
+        1 => 0b1011,
+        2 => 0b0000,
+        4 => 0b0001,
+        8 => 0b0010,
+        16 => 0b0011,
+        32 => 0b1000,
+        64 => 0b1001,
+        128 => 0b1010,
+        else => @panic("Invalid APIC divider"),
+    };
+    reg.write(value);
+}
+
+pub fn setInitialCount(value: u32) void {
+    const reg = lapic.Reg(u32, 0x380);
+    reg.write(value);
+}
+
+pub fn setCurrentCount(value: u32) void {
+    const reg = lapic.Reg(u32, 0x390);
+    reg.write(value);
+}
+
+pub fn getCurrentCount() u32 {
+    const reg = lapic.Reg(u32, 0x390);
+    return reg.read();
+}
+
+pub const TimerMode = enum(u2) {
+    OneShot = 0b00,
+    Periodic = 0b01,
+    TSCDeadline = 0b10,
+};
+
+pub const Mask = enum(u1) {
+    NotMasked = 0,
+    Masked = 1,
+};
+
+pub fn setTimerLVT(vector: u8, mask: Mask, mode: TimerMode) void {
+    const reg = lapic.Reg(u32, 0x320);
+    const value = vector | (@as(u32, @enumToInt(mask)) << 16) | (@as(u32, @enumToInt(mode)) << 17);
+    reg.write(value);
+}
+
 pub var lapic: MMIORegion = undefined;
 
 pub fn init() void {
